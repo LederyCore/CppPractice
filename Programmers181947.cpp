bce7f5d18c3be88b4396120614cc65344ee76616
LederyCore <lesb060500@gmail.com>
2025-09-18 16:31:35 +0900

    덧셈식 출력하기

---
+++ Programmers181947.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int solution()
+{
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << (a + b);
+    return 0;
+}
